1b973c11bce555ce1f8904209901ff41f87e83f2
srikanthgumma <srikanthgummadi516@gmail.com>
2023-08-02 16:26:56 +0530

    done changes

--- src/App.js
+++ src/App.js
@@ -4,115 +4,124 @@ import wpics from './images/wether.png';
 import './App.css';
 
 function App() {
-  let Wstyle ={
-    color:"black",
-    overflowY:"auto",
-    margin:"auto" ,
-    marginTop:"3%",
-    width:"850px",
-    border:"1px solid white",
-    padding:"10px",
-    borderRadius:"10px",
-    backgroundColor:"skyblue"
- }
-
- let d= new Date()
-  let dtime = d.getHours();
-  let dmin = d.getMinutes();
-
-let [b,setb]=useState('');
-let [a,seta] =useState([])
-
-
-let funwether= (q)=>{
-    q.preventDefault();
-    let city=q.target[0].value;
-    if(city !== ''){
-    setb(city);
-    } else{
-        alert("shold be empty")
+    let Wstyle = {
+        color: "black",
+        overflowY: "auto",
+        margin: "auto",
+        marginTop: "3%",
+        width: "850px",
+        border: "1px solid white",
+        padding: "10px",
+        borderRadius: "10px",
+        backgroundColor: "skyblue"
     }
-    console.log(city);
-    q.target[0].value = "";
-}
+
+    let d = new Date()
+    let dtime = d.getHours();
+    let dmin = d.getMinutes();
+
+    let [b, setb] = useState('');
+    let [a, seta] = useState([])
 
 
-useEffect(()=>{
- let key ='e4ef1914d07e1759b712293299504806';
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${b}&appid=${key}`).then((val)=>{
-       console.log(val.data);
-       seta([val.data])
-
-    },(err)=>{console.log(err);})
-
-},[b])
-console.log(a);    
-
-let dataa= a.map((value,index,array)=>{
-return (
-    <div style={{marginTop:"19px",height:'320px',position:"relative"}}>
-
-        <div style={{display:"flex",justifyContent:"space-around"}}>
-
-     <div style={{marginLeft:"-180px"}}>
-     <span>Current Weather</span><br />
-     <span>{dtime + ":"+dmin}PM</span>
-     </div>
-
-     <div>
-         <span style={{fontWeight:"bold"}}>{value.name}</span> <span>{Math.floor(value.main.temp-217.13)}  <sup>0</sup>C</span>
-
-    </div>          
-      </div>
-
-      <div style={{marginTop:"20px",display:"flex",textAlign:"center"}}>
-        <img src={wpics} alt="sunny" width="180px" height="180px"/> <span style={{fontSize:"7.5rem",marginLeft:"30px"}}>{Math.floor(value.main.temp-217-13)} <sup>oC</sup> </span>
-      </div>
+    let getWethearrReport = (q) => {
+        q.preventDefault();
+        let city = q.target[0].value;
+        if (city !== '') {
+            setb(city);
+        } else {
+            alert("shold be empty")
+        }
+        console.log(city);
+        q.target[0].value = "";
+    }
 
 
-        <div style={{display:"grid",gridTemplateColumns:"100px 100px 100px 100px 100px",justifyContent:"space-evenly",position:"absolute",bottom:"0",width:"100%"}} id="child">
-         <div>
-            <span>Air Quality</span><br />
-            <span>{value.wind.deg}</span>
-         </div>
+    useEffect(() => {
+        let key = 'e4ef1914d07e1759b712293299504806';
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${b}&appid=${key}`).then((val) => {
+            console.log(val);
+            if (val.status === 200) {
+                seta([val.data])
+            }
 
-         <div>
-            <span>Wind</span><br />
-            <span>{value.wind.speed}</span>
-         </div>
-         <div>
-            <span>Humidity</span><br />
-            <span>{value.main.humidity}</span>
-         </div>
-         
-         <div>
-            <span>Visibity</span><br />
-            <span>{value.visibility}</span>
-         </div>
+        }, (err) => {
+            console.log(err);
+            if (err.response.data.cod === "404") {
+                alert(err.response.data.message + ",  please enter a valid city name..");
+                seta([])
+            }
 
-         <div>
-            <span>Pressure</span><br />
-            <span>{value.main.pressure}</span>
-         </div>
+        })
 
+    }, [b])
+    console.log(a);
+
+    let dataa = a.map((value, index, array) => {
+        return (
+            <div style={{ marginTop: "19px", height: '320px', position: "relative" }}>
+
+                <div style={{ display: "flex", justifyContent: "space-around" }}>
+
+                    <div style={{ marginLeft: "-180px" }}>
+                        <span>Current Weather</span><br />
+                        <span>{dtime + ":" + dmin}PM</span>
+                    </div>
+
+                    <div>
+                        <span style={{ fontWeight: "bold" }}>{value.name}</span> <span>{Math.floor(value.main.temp - 273)}  <sup>0</sup>C</span>
+
+                    </div>
+                </div>
+
+                <div style={{ marginTop: "20px", display: "flex", textAlign: "center" }}>
+                    <img src={wpics} alt="sunny" width="180px" height="180px" /> <span style={{ fontSize: "7.5rem", marginLeft: "30px" }}>{Math.floor(value.main.temp - 273)} <sup>oC</sup> </span>
+                </div>
+
+
+                <div style={{ display: "grid", gridTemplateColumns: "100px 100px 100px 100px 100px", justifyContent: "space-evenly", position: "absolute", bottom: "0", width: "100%" }} id="child">
+                    <div>
+                        <span>Air Quality</span><br />
+                        <span>{value.wind.deg}</span>
+                    </div>
+
+                    <div>
+                        <span>Wind</span><br />
+                        <span>{value.wind.speed}</span>
+                    </div>
+                    <div>
+                        <span>Humidity</span><br />
+                        <span>{value.main.humidity}</span>
+                    </div>
+
+                    <div>
+                        <span>Visibity</span><br />
+                        <span>{value.visibility}</span>
+                    </div>
+
+                    <div>
+                        <span>Pressure</span><br />
+                        <span>{value.main.pressure}</span>
+                    </div>
+
+                </div>
+
+
+            </div>
+        )
+    })
+    return (
+        <div  >
+            <h1 style={{ color: "black", margin: "auto", width: "280px", paddingTop: "100px" }}>Weather App</h1>
+            <div style={Wstyle}>
+                <form onSubmit={getWethearrReport} style={{ width: "100%" }}>
+                    <input type="text" name="tex" id="pop" placeholder="Enter any city name to check for weather details" style={{ width: "75%", height: "2.4rem", color: "black" }} />
+                    <input type="submit" value="SEARCH" style={{ height: "2.4rem", width: "25%" }} />
+                </form>
+                <div >{dataa}</div>
+            </div>
         </div>
-
-
-    </div>
-)
-})
-return (
-    <div >
-        <h1 style={{color:"black",margin:"auto",width:"280px",paddingTop:"100px"}}>Weather App</h1>
-        <div style={Wstyle}>
-        <form onSubmit={funwether} style={{width:"100%"}}>
-            <input type="text" name="tex" id="pop" placeholder="Enter any city name to check for weather details" style={{width:"75%",height:"2.4rem",color:"black"}}/>
-            <input type="submit" value="SEARCH" style={{height:"2.4rem",width:"25%"}} />
-        </form>
-        <div>{dataa}</div>
-        </div>
-    </div>
-)
+    )
 }
 
 export default App;
